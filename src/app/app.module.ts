bf1801893078c9df2db3097a0f7b7d63c08b1080
JuanZu77 <juanvzubiri77@gmail.com>
2024-03-20 22:09:20 -0300

    primera versión del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/modules/dashboard/router-child.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+export const childRoutes: Routes = [
+
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+]
+
+@NgModule({
+    imports: [RouterModule.forChild(childRoutes)],
+    exports: [RouterModule],
+})
+export class RouterChildModule { }
